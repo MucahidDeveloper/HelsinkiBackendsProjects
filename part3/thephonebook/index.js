63b8c6d45d0e674c97fe72312c793f7cbe59576b
MucahidDeveloper <Mucadad221@gmail.com>
2025-03-03 01:25:53 +0300

    solving 3.8: Phonebook backend step 8 exercise - make morgan show the contents of POST requests

--- part3/thephonebook/index.js
+++ part3/thephonebook/index.js
@@ -3,7 +3,15 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("post-data", (req) =>
+  req.method === "POST" ? JSON.stringify(req.body) : ""
+);
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-data"
+  )
+);
 
 let persons = [
   {
