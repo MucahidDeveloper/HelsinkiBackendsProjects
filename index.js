cbaabd28072ae7cd387e1a1f0a5e85e4cdafc4ce
MucahidDeveloper <Mucadad221@gmail.com>
2025-03-03 03:18:19 +0300

    fixing bug

--- index.js
+++ index.js
@@ -96,6 +96,13 @@ app.get("/info", (request, response) => {
 
 app.use(express.static("dist"));
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
