f14074d7f299120b8e231186edf49cb682264c5b
MucahidDeveloper <Mucadad221@gmail.com>
2025-03-13 17:18:01 +0300

    exercise 3.13 - module for new person

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+require("dotenv").config();
 
-const cors = require("cors");
-app.use(cors());
-
-app.use(express.json());
+const Person = require("./models/person");
 
 morgan.token("post-data", (req) =>
   req.method === "POST" ? JSON.stringify(req.body) : ""
@@ -16,55 +14,32 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
-  console.log(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
 });
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
-});
-
-const generateId = () => {
-  const id = Math.random().toFixed(5) * 100000;
-  return String(id);
-};
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -77,22 +52,38 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({ error: "Name must be unique" });
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+  // persons = persons.concat(person);
 
-  response.status(201).json(person);
+  // response.status(201).json(person);
 });
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+// const generateId = () => {
+//   const id = Math.random().toFixed(5) * 100000;
+//   return String(id);
+// };
 
 app.get("/info", (request, response) => {
   const time = new Date().toString();
   const count = persons.length;
   response.send(`<p>Phonebook has info for ${count} people</p><p>${time}</p>`);
 });
+
+app.use(unknownEndpoint);
 
 app.use(express.static("dist"));
 
