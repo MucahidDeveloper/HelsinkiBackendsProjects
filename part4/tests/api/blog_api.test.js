cce3aca629d7c7162bdf3e43e5d34430ae4a035d
MucahidDeveloper <Mucadad221@gmail.com>
2025-03-18 01:49:37 +0300

    4.10: Blog List Tests step 3 - change promises to async/await

--- part4/tests/api/blog_api.test.js
+++ part4/tests/api/blog_api.test.js
@@ -12,7 +12,7 @@ test("blogs are returned as json", async () => {
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
-  assert.strictEqual(response.body.length, 3);
+  // assert.strictEqual(response.body.length, 3);
 
   const titles = response.body.map((e) => e.title);
   assert(titles.includes("Test1"));
@@ -22,6 +22,28 @@ test("blog posts have an id property instead of _id", async () => {
   const response = await api.get("/api/blogs");
   assert(response.body[0].id);
   assert(!response.body[0]._id);
+});
+
+test("a valid blog can be added ", async () => {
+  const newBlog = {
+    title: "Test4",
+    author: "testRequest",
+    url: "test.com",
+    likes: 4,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body.length, 8);
+
+  const titles = response.body.map((e) => e.title);
+  assert(titles.includes("Test4"));
 });
 
 after(async () => {
