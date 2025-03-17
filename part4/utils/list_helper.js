cd1a4a81a59071d063d279535e61c7b0c2edc27e
MucahidDeveloper <Mucadad221@gmail.com>
2025-03-17 08:31:14 +0300

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,25 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  let favorite = blogs[0];
+  for (const blog of blogs) {
+    if (blog.likes > favorite.likes) {
+      favorite = blog;
+    }
+  }
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
