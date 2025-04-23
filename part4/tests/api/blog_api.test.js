8e939501a5778c622f9041036886b979029ddf00
MucahidDeveloper <Mucadad221@gmail.com>
2025-04-23 16:52:50 +0300

    solving 4.21* & 4.22*: Blog List Expansion, step 10+11 exercises

--- part4/tests/api/blog_api.test.js
+++ part4/tests/api/blog_api.test.js
@@ -5,20 +5,35 @@ const supertest = require("supertest");
 const app = require("../../app");
 const Blog = require("../../models/blog");
 const helper = require("../api/test_helper.test.js");
+const jwt = require("jsonwebtoken");
 const { url } = require("inspector");
 const { title } = require("process");
 
 const api = supertest(app);
 
+let token;
+
+beforeEach(async () => {
+  // إنشاء مستخدم جديد مع توليد توكين له
+  const user = {
+    username: "testuser",
+    name: "Test User",
+    passwordHash: "password123",
+  };
+
+  const savedUser = await api.post("/api/users").send(user);
+
+  // توليد التوكين
+  token = jwt.sign({ id: savedUser.body.id }, process.env.SECRET);
+
+  await Blog.deleteMany({});
+  console.log("cleared");
+
+  await Blog.insertMany(helper.initialBlogs);
+  console.log("inserted");
+});
+
 describe("Blog Requests", () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({});
-    console.log("cleared");
-
-    await Blog.insertMany(helper.initialBlogs);
-    console.log("inserted");
-  });
-
   test("blogs are returned as json", async () => {
     const response = await api
       .get("/api/blogs")
@@ -37,7 +52,7 @@ describe("Blog Requests", () => {
     assert(!response.body[0]._id);
   });
 
-  test("a valid blog can be added", async () => {
+  test("a valid blog can be added with a valid token", async () => {
     const newBlog = {
       title: "Test4",
       author: "testRequest",
@@ -47,6 +62,7 @@ describe("Blog Requests", () => {
 
     await api
       .post("/api/blogs")
+      .set("Authorization", `Bearer ${token}`)
       .send(newBlog)
       .expect(201)
       .expect("Content-Type", /application\/json/);
@@ -59,9 +75,23 @@ describe("Blog Requests", () => {
     assert(titles.includes("Test4"));
   });
 
-  test.only("blog likes will be 0 if not specified", async () => {
+  test("blog adding without a token should fail with 401", async () => {
+    const newBlog = {
+      title: "Test4",
+      author: "testRequest",
+      url: "test.com",
+      likes: 4,
+    };
+
+    const response = await api.post("/api/blogs").send(newBlog).expect(401);
+
+    assert.strictEqual(response.body.error, "Token missing or invalid");
+  });
+
+  test("blog likes will be 0 if not specified", async () => {
     await api
       .post("/api/blogs")
+      .set("Authorization", `Bearer ${token}`)
       .send(helper.blogWithNoLikes)
       .expect(201)
       .expect("Content-Type", /application\/json/);
@@ -71,7 +101,7 @@ describe("Blog Requests", () => {
     assert.strictEqual(blog.likes, 0);
   });
 
-  test.only("blog without title or url is not added", async () => {
+  test("blog without title or url is not added", async () => {
     const newBlogWithoutTitle = {
       author: "testRequest",
       url: "test.com",
@@ -84,15 +114,27 @@ describe("Blog Requests", () => {
       likes: 4,
     };
 
-    await api.post("/api/blogs").send(newBlogWithoutTitle).expect(400);
-    await api.post("/api/blogs").send(newBlogWithoutUrl).expect(400);
+    await api
+      .post("/api/blogs")
+      .set("Authorization", `Bearer ${token}`)
+      .send(newBlogWithoutTitle)
+      .expect(400);
+
+    await api
+      .post("/api/blogs")
+      .set("Authorization", `Bearer ${token}`)
+      .send(newBlogWithoutUrl)
+      .expect(400);
   });
 
-  test("a blog can be deleted", async () => {
+  test("a blog can be deleted with a valid token", async () => {
     const response = await api.get("/api/blogs");
     const blogToDelete = response.body[0];
 
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(204);
 
     const blogsAtEnd = await helper.blogsInDb();
     assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
@@ -101,7 +143,18 @@ describe("Blog Requests", () => {
     assert(!titles.includes(blogToDelete.title));
   });
 
-  test("a blog can be updated", async () => {
+  test("deleting a blog without a token should fail with 401", async () => {
+    const response = await api.get("/api/blogs");
+    const blogToDelete = response.body[0];
+
+    const deleteResponse = await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(401);
+
+    assert.strictEqual(deleteResponse.body.error, "Token missing or invalid");
+  });
+
+  test("a blog can be updated with a valid token", async () => {
     const response = await api.get("/api/blogs");
     const blogToUpdate = response.body[0];
 
@@ -114,6 +167,7 @@ describe("Blog Requests", () => {
 
     await api
       .put(`/api/blogs/${blogToUpdate.id}`)
+      .set("Authorization", `Bearer ${token}`)
       .send(updatedBlog)
       .expect(200)
       .expect("Content-Type", /application\/json/);
@@ -131,6 +185,7 @@ describe("Blog Requests", () => {
 
     await api
       .put(`/api/blogs/${blogToUpdate.id}`)
+      .set("Authorization", `Bearer ${token}`)
       .send(updatedBlogWithoutLikes)
       .expect(400);
   });
