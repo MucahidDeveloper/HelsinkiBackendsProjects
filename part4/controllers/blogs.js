7480b432b52a4d4c0f619c4b87876f50f734f246
MucahidDeveloper <Mucadad221@gmail.com>
2025-04-23 16:05:25 +0300

    solving 4.19: Blog List Expansion, step 7 exercise - New blog require valid token

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,6 +2,8 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
+const jwt = require("jsonwebtoken");
+
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
@@ -17,26 +19,40 @@ blogsRouter.get("/:id", async (request, response, next) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-
-    user: user._id,
-  });
-
   try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token invalid" });
+    }
+
+    const user = await User.findById(decodedToken.id);
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user._id,
+    });
+
     const savedBlog = await blog.save();
     user.blogs = user.blogs.concat(savedBlog._id);
     await user.save();
+
     response.status(201).json(savedBlog);
   } catch (error) {
-    next(error);
+    return response.status(401).json({ error: "token invalid or missing" });
   }
 });
 
